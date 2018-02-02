2d582f72af13fb288922d12988aff6ca4a781071
Ahmad Arif <ahmad.arif019@gmail.com>
2018-02-02 17:29:02 +0700

    fix table user schema

--- database/migrations/1503248427885_user.js
+++ database/migrations/1503248427885_user.js
@@ -6,9 +6,9 @@ class UserSchema extends Schema {
   up () {
     this.create('users', table => {
       table.increments()
-      table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
+      table.string('username', 80).notNullable()
       table.timestamps()
     })
   }
